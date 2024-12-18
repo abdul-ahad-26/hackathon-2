98f4623950d310e606bff320ba0dfab3ec0bc8e8
abdul-ahad-26 <hayymalik678@gmail.com>
2024-12-19 00:50:28 +0500

    corrected errors and made editors pick

--- src/app/components/EditorsPick.tsx
+++ src/app/components/EditorsPick.tsx
@@ -3,17 +3,17 @@ import React from 'react'
 const EditorsPick = () => {
     return (
         <div className='flex flex-col w-full px-4 items-center justify-center'>
-            <div className='bg-cover bg-center w-[323px]  h-[500px] ' style={{
+            <div className='bg-cover bg-center row-span-1 ' style={{
                 backgroundImage: "url('/men.png')"
             }}></div>
-            <div className='bg-cover bg-center w-[323px] h-[500px]' style={{
+            <div className='bg-cover bg-center row-span-1' style={{
                 backgroundImage: "url('/women.png')"
             }}></div>
             <div className="flex flex-col gap-2">
-            <div className='bg-cover bg-center w-[323px] h-[240px]' style={{
+            <div className='bg-cover bg-center row-span-1' style={{
                 backgroundImage: "url('/accessories.png')"
             }}></div>
-            <div className='bg-cover bg-center w-[323px] h-[240px]' style={{
+            <div className='bg-cover bg-center row-span-1' style={{
                 backgroundImage: "url('/kids.png')"
             }}></div>
 
